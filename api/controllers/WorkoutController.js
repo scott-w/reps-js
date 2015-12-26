e9525c8f2a7fc1b51b0da503ab9b60b0e49dc21b
Scott Walton <s@cott.me.uk>
2015-12-26 15:26:50 +0000

    Some nice callback hell
    Hacked in some callback hell for our create workout+set logic.

--- api/controllers/WorkoutController.js
+++ api/controllers/WorkoutController.js
@@ -15,9 +15,13 @@ module.exports = {
    * `WorkoutController.list()`
    */
   list: function (req, res) {
-    return res.json({
-      todo: 'list() is not implemented yet!'
-    });
+		Workout.find({
+			where: {
+				user: req.session.user.id
+			}
+		}).exec(function(err, workouts) {
+			res.json(workouts);
+		});
   },
 
 
@@ -26,6 +30,7 @@ module.exports = {
    */
   create: function (req, res) {
 		var user = req.session.user;
+
 		Workout.create({
 			workout_date: moment(req.body.workout_date, 'DD/MM/YYYY').toDate(),
 			user: user.id
@@ -34,7 +39,18 @@ module.exports = {
 				return res.status(400).json(err.invalidAttributes);
 			}
 
-			return res.status(201).json(created);
+			var sets = _.each(_.clone(req.body.sets || []), function(item) {
+				item.workout = created.id;
+			});
+
+			if (sets) {
+				Set.create(sets).exec(function(setErr, createdSet) {
+					return res.status(201).json(createdSet);
+				});
+			}
+			else {
+				return res.status(201).json(created);
+			}
 		});
   },
 
