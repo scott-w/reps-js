398135a40ab00a9fd11c13e6e0adc5b3810f9d44
Scott Walton <s@cott.me.uk>
2016-02-14 21:58:30 +0000

    Setup the sync auth handler #25

--- app/workouts/models/workout.js
+++ app/workouts/models/workout.js
@@ -1,8 +1,11 @@
 import moment from 'moment';
 import Backbone from 'backbone';
 
+import {authSync} from '../../base/models/auth';
+
 export default Backbone.Model.extend({
   idAttribute: 'workout_date',
+  sync: authSync,
 
   formatDate: function() {
     return moment(this.get('workout_date')).fromNow();
