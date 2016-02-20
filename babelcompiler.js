43b859126fc47ed5a160bef6a35b1d50357a984e
Scott Walton <s@cott.me.uk>
2016-02-20 08:11:12 +0000

    Used the mock-browser to get jquery.ajax to work #28

--- babelcompiler.js
+++ babelcompiler.js
@@ -1,19 +1,12 @@
-const _ = require('lodash');
 const Babel = require('babel-core');
 
 const MockBrowser = require('mock-browser').mocks.MockBrowser;
 const browser = new MockBrowser();
 
-global.window = {
-  localStorage: browser.getLocalStorage(),
-  location: {
-    protocol: 'http'
-  }
-};
-
-_.each(global.window, (value, key) => {
-  global[key] = value;
-});
+global.window = browser.getWindow();
+global.window.localStorage = browser.getLocalStorage();
+global.window.location = browser.getLocation();
+global.localStorage = global.window.localStorage;
 
 module.exports = [{
   ext: '.js',
