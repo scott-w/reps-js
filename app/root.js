1d007c20613cdf37b0157eb49381447d6632c499
Scott Walton <s@cott.me.uk>
2016-02-22 20:20:57 +0000

    Added nav at the top #28

--- app/root.js
+++ app/root.js
@@ -1,8 +1,39 @@
+import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 
 const Nav = Marionette.View.extend({
   className: 'container-fluid',
-  template: require('./templates/nav.html')
+  template: require('./templates/nav.html'),
+
+  ui: {
+    root: '.root',
+    exercise: '.exercise',
+    workout: '.workout'
+  },
+
+  events: {
+    'click @ui.root': 'showRoot',
+    'click @ui.exercise': 'showExercise',
+    'click @ui.workout': 'showWorkout'
+  },
+
+  showRoot: function() {
+    this._navigate('');
+  },
+
+  showExercise: function(e) {
+    e.preventDefault();
+    this._navigate('exercise/');
+  },
+
+  showWorkout: function(e) {
+    e.preventDefault();
+    this._navigate('workout/');
+  },
+
+  _navigate: function(url) {
+    Backbone.history.navigate(url, {trigger: true});
+  }
 });
 
 const Layout = Marionette.View.extend({
