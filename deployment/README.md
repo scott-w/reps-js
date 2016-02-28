281bdcbb2d375ee5fe4b441c288c3d2c27548432
Scott Walton <s@cott.me.uk>
2016-02-28 08:52:28 +0000

    Added a simple deployment script #54

--- deployment/README.md
+++ deployment/README.md
@@ -2,3 +2,8 @@
 
 The templates here can be used to setup a deployment. I'll worry about
 automating these steps later.
+
+## Start a deployment
+
+To deploy the system, run `python deploy.py` - you shouldn't need to install
+any dependencies.
---
+++ deployment/deploy.py
@@ -0,0 +1,26 @@
+from os.path import isfile
+from subprocess import Popen
+
+
+def update_app():
+    Popen(['sudo', '-u www', 'git', 'pull'], cwd='/home/www/reps-js')
+    Popen(
+        ['sudo', '-u www', 'npm', 'i', '--no-progress'],
+        cwd='/home/www/reps-js')
+    Popen(
+        ['sudo', '-u www', 'npm', 'run-script', 'compile'],
+        cwd='/home/www/reps-js')
+
+
+def check_upstart():
+    return isfile('/etc/init/pumped.conf')
+
+
+def restart_app():
+    Popen(['sudo', 'service', 'pumped', 'restart'])
+
+
+if __name__ == '__main__':
+    update_app()
+    if check_upstart():
+        restart_app()
