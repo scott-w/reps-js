a8b141321f8514b6486bb7ddf8282eada655b7f2
Scott Walton <s@cott.me.uk>
2016-03-26 08:01:42 +0000

    Added the outline for a notification system #84
    Added the model/collection/view outline for a notification system using
    the Radio. We’ll see how well it works when we pull it together.

---
+++ test/app/test_notification.js
@@ -0,0 +1,64 @@
+const Code = require('code');
+const Lab = require('lab');
+const sinon = require('sinon');
+
+const lab = exports.lab = Lab.script();
+
+const after = lab.after;
+const afterEach = lab.afterEach;
+const before = lab.before;
+const beforeEach = lab.beforeEach;
+const describe = lab.describe;
+const expect = Code.expect;
+const it = lab.it;
+
+import {NotificationList} from '../../app/base/collections/notification';
+
+
+describe('Notification List', () => {
+  let notifications;
+
+  beforeEach((done) => {
+    notifications = new NotificationList();
+    done();
+  });
+
+  afterEach((done) => {
+    notifications = null;
+    done();
+  });
+
+  it('adds success messages', (done) => {
+    notifications.addSuccessMsg('test');
+
+    expect(notifications.length).to.equal(1);
+    const model = notifications.first();
+
+    expect(model.get('alert_type')).to.equal('success');
+    expect(model.get('alert_text')).to.equal('test');
+    done();
+  });
+
+  it('adds error messages', (done) => {
+    notifications.addErrorMsg('test');
+
+    expect(notifications.length).to.equal(1);
+    const model = notifications.first();
+
+    expect(model.get('alert_type')).to.equal('error');
+    expect(model.get('alert_text')).to.equal('test');
+    done();
+  });
+
+  it('adds warning messages', (done) => {
+    notifications.addWarningMsg('test');
+
+    expect(notifications.length).to.equal(1);
+    const model = notifications.first();
+
+    expect(model.get('alert_type')).to.equal('warning');
+    expect(model.get('alert_text')).to.equal('test');
+    done();
+  });
+
+});
