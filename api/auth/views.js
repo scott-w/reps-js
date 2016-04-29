0e0685bc6a7c3261907336da62da5a8576e57fb7
Scott Walton <s@cott.me.uk>
2016-04-29 11:29:51 +0100

    Fixed critical bug that logged any user in #21

--- api/auth/views.js
+++ api/auth/views.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt');
 const models = require('../../models');
 
 const jwt = require('./jwt');
+const forms = require('./forms');
 
 
 const getUserByEmail = function (email) {
@@ -21,24 +22,25 @@ const getUserByEmail = function (email) {
 const token = function (request, reply) {
   getUserByEmail(request.query.email).then(function (result) {
     if (result) {
-      bcrypt.compare(request.query.password, result.password, (err) => {
-        if (err) {
+      const user = result.dataValues;
+      bcrypt.compare(request.query.password, user.password, (err, match) => {
+        if (match) {
           return reply({
-            password: 'Incorrect'
-          }).code(400);
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            scope: 'all',
+            token: jwt.getToken(result)
+          });
         }
 
-        reply({
-          email: result.email,
-          first_name: result.first_name,
-          last_name: result.last_name,
-          scope: 'all',
-          token: jwt.getToken(result)
-        });
+        return reply({
+          password: 'Incorrect'
+        }).code(400);
       });
     }
     else {
-      reply({
+      return reply({
         password: 'Incorrect'
       }).code(400);
     }
@@ -48,41 +50,50 @@ const token = function (request, reply) {
 
 /** Create a new user */
 const createUser = function (request, reply) {
+  const createErrors = forms.createUserError(request.payload);
+
+  if (createErrors) {
+    return reply(createErrors).code(400);
+  }
   getUserByEmail(request.payload.email).then((result) => {
-      if (result) {
-        reply({
-            email: 'Already exists'
+    if (result) {
+      return reply({
+          email: 'Already exists'
+        }).code(400);
+
+    }
+    else {
+      const email = request.payload.email;
+      const password = request.payload.password;
+      const first_name = request.payload.first_name;
+      const last_name = request.payload.last_name;
+
+      bcrypt.hash(password, bcrypt.genSaltSync(10), (err, hash) => {
+        if (err) {
+          return reply({
+            error: 'An error occurred with your password'
           }).code(400);
-
-      } else {
-        const email = request.payload.email;
-        const password = request.payload.password;
-        const first_name = request.payload.first_name;
-        const last_name = request.payload.last_name;
-
-        bcrypt.hash(password, bcrypt.genSaltSync(10), (err, hash) => {
-          if (err) {
-            return reply({
-              error: 'An error occurred with your password'
-            }).code(400);
-          }
-          models.User.create({
-            email: email,
-            password: hash,
-            first_name: first_name,
-            last_name: last_name
-          }).then((instance) => {
-              reply({
-                email: instance.email,
-                id: instance.id,
-                first_name: instance.first_name,
-                last_name: instance.last_name,
-                token: jwt.getToken(instance)
-              }).code(201);
-            });
+        }
+        models.User.create({
+          email: email,
+          password: hash,
+          first_name: first_name,
+          last_name: last_name
+        }).then((instance) => {
+          reply({
+            email: instance.email,
+            id: instance.id,
+            first_name: instance.first_name,
+            last_name: instance.last_name,
+            token: jwt.getToken(instance)
+          }).code(201);
+        }).catch(err => {
+          console.error(err);
+          reply({error: 'An error occurred'}).code(500);
         });
-      }
-    });
+      });
+    }
+  });
 };
 
 
--- test/api/test_user.js
+++ test/api/test_user.js
@@ -74,6 +74,56 @@ describe('Create user', () => {
       done();
     });
   });
+
+  it('must take a password at least 8 long', done => {
+    const reqData = {
+      method: 'post',
+      url: '/user/',
+      payload: {
+        email: "test@example.com",
+        password: "short",
+        first_name: "First",
+        last_name: "Last"
+      }
+    };
+
+    server.inject(reqData, (response) => {
+      expect(response.statusCode).to.equal(400);
+      expect(
+        response.headers['content-type'].split(';')[0]
+      ).to.equal('application/json');
+      expect(response.result.password.length).to.equal(1);
+      expect(
+        response.result.password[0]).to.equal(
+        "Password is too short (minimum is 8 characters)");
+      done();
+    });
+  });
+
+  it('must have an email address', done => {
+    const reqData = {
+      method: 'post',
+      url: '/user/',
+      payload: {
+        password: "short",
+        first_name: "First",
+        last_name: "Last"
+      }
+    };
+
+    server.inject(reqData, (response) => {
+      expect(response.statusCode).to.equal(400);
+      expect(
+        response.headers['content-type'].split(';')[0]
+      ).to.equal('application/json');
+      expect(response.result.email.length).to.equal(1);
+      expect(
+        response.result.email[0]).to.equal(
+        "Email can't be blank");
+      done();
+    });
+  });
+
 });
 
 
@@ -94,11 +144,26 @@ describe('Login user', () => {
       done();
     });
   });
+
+  it('refuses to login invalid credentials', (done) => {
+    const req = {
+      url: '/token?email=test@example.com&password=nosuchpassword'
+    };
+
+    server.inject(req, response => {
+      console.log('\n\n\n\n\n\n');
+      console.log(response.result);
+      expect(response.statusCode).to.equal(400);
+      expect(response.result.password).to.equal('Incorrect');
+      done();
+    });
+  });
+
 });
 
 
 describe('Update user', () => {
-  beforeEach((done) => {
+  beforeEach(done => {
     construct.fixtures('./fixtures/users.yaml', done);
   });
 
