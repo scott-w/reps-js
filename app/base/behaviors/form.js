dfa8953660f45f13a78137807eed020f6236bef8
Scott Walton <s@cott.me.uk>
2016-05-22 17:55:47 +0100

    Added offline validation behaviours #109
    Added a behaviour for handling offline validation and generalised out
    the classes a little bit - we could use a different template if we
    liked - though it would take a little more work to fit together.

--- app/base/behaviors/form.js
+++ app/base/behaviors/form.js
@@ -20,18 +20,11 @@ export const Validation = Behavior.extend({
   }
 });
 
-/** The FormError is able to display popovers on a form from the server.
-    To use it, just pass an object of:
-      serverField -> input-selector
-    and this will render the popover next to it.
-*/
-export const FormError = Behavior.extend({
-  modelEvents: {
-    error: 'showFieldErrors'
-  },
 
+const _BaseValidationBehavior = Behavior.extend({
   options: {
-    errors: {}
+    errors: {},
+    popoverClass: '.form-error'
   },
 
   ui: function() {
@@ -43,26 +36,67 @@ export const FormError = Behavior.extend({
     'keydown': 'hidepopover'
   },
 
-  /** Renders each field error received from the server in a popover */
-  showFieldErrors: function(model, response) {
-    _.each(response.responseJSON, (value, attribute) => {
-      const ui = this.getUI(attribute);
-
-      if (_.isArray(value)) {
-        value = value[0];
-      }
-      const output = this.getPopoverTemplate(value);
-      ui.parent().append(output);
-    });
-  },
-
   /** Destroys all popovers */
   hidepopover: function() {
-    this.$('.form-error').remove();
+    const formError = this.getOption('popoverClass');
+    this.$(formError).remove();
   },
 
   /** Gets a rendered template of the popover to display */
   getPopoverTemplate: function(content) {
-    return popoverTemplate({content: content});
+    return popoverTemplate({
+      content: content,
+      popoverClass: this.getOption('popoverClass')
+    });
+  },
+
+  /** Render the error against the UI referenced by key */
+  showError: function(value, attribute) {
+    const ui = this.getUI(attribute);
+
+    if (_.isArray(value)) {
+      value = value[0];
+    }
+    const output = this.getPopoverTemplate(value);
+    ui.parent().append(output);
+  }
+});
+
+
+/** Display validation errors from the model on the view as popovers.
+    To use this, just pass an object of:
+      modelField -> input-selector
+    and this will render the popover next to the field.
+
+    This is used for client-thrown invalidation errors.
+    For this to work, `model.validate` must return an object - this will do
+    nothing if any other type is returned e.g. array or string
+*/
+export const ValidationError = _BaseValidationBehavior.extend({
+  modelEvents: {
+    invalid: 'showFieldErrors'
+  },
+
+  showFieldErrors: function(model, errors) {
+    _.each(errors, this.showError, this);
+  }
+});
+
+
+/** The FormError is able to display popovers on a form from the server.
+    To use it, just pass an object of:
+      serverField -> input-selector
+    and this will render the popover next to it.
+
+    This will render the server-sent validation errors.
+*/
+export const FormError = _BaseValidationBehavior.extend({
+  modelEvents: {
+    error: 'showFieldErrors'
+  },
+
+  /** Renders each field error received from the server in a popover */
+  showFieldErrors: function(model, response) {
+    _.each(response.responseJSON, this.showError, this);
   }
 });
--- test/app/errors.test.js
+++ test/app/errors.test.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import {Model} from 'backbone';
 import {View} from 'backbone.marionette';
 
-import {FormError} from '../../app/base/behaviors/form';
+import {FormError, ValidationError} from '../../app/base/behaviors/form';
 
 
 describe('Error handler', function() {
@@ -110,6 +110,96 @@ describe('Error handler', function() {
         my_attribute: ['Was not valid']
       }
     });
+
+    expect(behavior.getPopoverTemplate.calledWith('Was not valid')).to.be(true);
+  });
+
+  it('clears errors when the user does something', function() {
+    const behavior = _.reduce(view._behaviors, (memo, behavior) => {
+      if (memo) {
+        return memo;
+      }
+      return behavior.getPopoverTemplate ? behavior : false;
+    }, false);
+
+    const remove = sinon.stub();
+    sinon.stub(behavior, '$', () => ({remove: remove}));
+
+    view.$el.trigger('click');
+
+    expect(behavior.$.calledWith('.form-error')).to.be(true);
+    expect(remove.calledOnce).to.be(true);
+  });
+});
+
+
+describe('Validation handler', function() {
+  const MyModel = Model.extend({
+    validate: () => ({my_attribute: 'Was not valid'})
+  });
+  let model;
+
+  const MyView = View.extend({
+    behaviors: {
+      error: {
+        behaviorClass: ValidationError,
+        errors: {
+          my_attribute: '.attribute'
+        }
+      }
+    }
+  });
+  let view;
+  let append;
+
+  beforeEach(function() {
+    model = new MyModel();
+
+    view = new MyView({model: model});
+
+    append = sinon.stub();
+
+    _.each(view._behaviors, behavior => {
+      let ui = {
+        parent: () => ({
+          append: append
+        })
+      };
+      sinon.stub(behavior, 'getUI', () => ui);
+    });
+  });
+
+  afterEach(function() {
+    _.each(view._behaviors, behavior => {
+      behavior.getUI.restore();
+    });
+    model = null;
+    view = null;
+    append = null;
+  });
+
+  it('sets up the ui', function() {
+    _.each(view._behaviors, behavior => {
+      expect(behavior.ui.my_attribute).to.equal('.attribute');
+    });
+  });
+
+  it('fires error handlers on the view', function() {
+    model.set('my_attribute', 'Some value', {validate: true});
+
+    expect(append.calledOnce).to.equal(true);
+  });
+
+  it('renders the template with the right data', function() {
+    const behavior = _.reduce(view._behaviors, (memo, behavior) => {
+      if (memo) {
+        return memo;
+      }
+      return behavior.getPopoverTemplate ? behavior : false;
+    }, false);
+    sinon.stub(behavior, 'getPopoverTemplate', () => '');
+
+    model.set('my_attribute', 'Some value', {validate: true});
 
     expect(behavior.getPopoverTemplate.calledWith('Was not valid')).to.be(true);
   });
