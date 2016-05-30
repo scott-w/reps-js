4728a52cad4856e9e1b5a72b7cefeb5dcdc4cd87
Scott Walton <s@cott.me.uk>
2016-05-30 19:02:15 +0100

    Put together the loader behaviour #110
    Put together a loader that will determine the right views to show in
    the collection views. It just needs to be injected into the relevant
    areas now.

--- app/base/behaviors/loader.js
+++ app/base/behaviors/loader.js
@@ -1,3 +1,4 @@
+import _ from 'underscore';
 import {Behavior} from 'backbone.marionette';
 
 
@@ -8,9 +9,9 @@ import {Behavior} from 'backbone.marionette';
 export const LoadingBehavior = Behavior.extend({
   defaults: {
     collectionRegion: '.collection-hook',
-    loadView: null,
-    collectionView: null,
-    emptyView: null,
+    loadView: undefined,
+    collectionView: undefined,
+    emptyView: undefined,
     initialState: 'loading'
   },
 
@@ -19,15 +20,76 @@ export const LoadingBehavior = Behavior.extend({
     request: 'startLoading'
   },
 
-  /** Determine whether to show the loading view or empty view.
-  */
-  completeLoading: function() {
+  initialize: function() {
+    const keys = ['loadView', 'collectionView', 'emptyView'];
+    const notSet = _.filter(keys, key => !this.getOption(key));
 
+    if (notSet.length) {
+      const error = notSet.join(', ');
+      throw `Behavior keys not set: ${error}`;
+    }
+  },
+
+  onRender: function() {
+    if (this.view.collection.length) {
+      this.showCollectionView();
+    }
+    else {
+      this._showEmptyOrLoading();
+    }
+  },
+
+  /** Determine whether to show the collection view or empty view.
+  */
+  completeLoading: function(collection) {
+    if (collection.length) {
+      this.showCollectionView();
+    }
+    else {
+      this.showEmpty();
+    }
+  },
+
+  /** Show the loaded collection view
+  */
+  showCollectionView: function() {
+    this.showInRegion(this.getOption('collectionView'));
   },
 
   /** Set the loading widget
   */
   startLoading: function() {
+    this.showInRegion(this.getOption('loadView'));
+  },
 
+  /** Show a given view in the collection region
+  */
+  showInRegion: function(View) {
+    this.view.showChildView(this.getOption('collectionRegion'), new View({
+      collection: this.view.collection
+    }));
+  },
+
+  /** Show the empty widget
+  */
+  showEmpty: function() {
+    this.showInRegion(this.getOption('emptyView'));
+  },
+
+  /** Using the options, show either the loading widget or the empty view - this
+      assumes that the collection is empty
+  */
+  _showEmptyOrLoading: function() {
+    const initial = this.getOption('initialState');
+
+    if (initial === 'loading') {
+      this.startLoading();
+    }
+    else if (initial === 'empty') {
+      this.showEmpty();
+    }
+    else {
+      throw 'Initial must be loading or empty';
+    }
   }
 });
